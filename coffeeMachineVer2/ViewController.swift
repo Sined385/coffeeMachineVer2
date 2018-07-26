e58c9138fc814d6e1be89b5c9008b7c9edb6eb12
Денис Ефремов <mac@denyska.local>
2018-07-26 22:26:36 +0300

    almost ready

--- coffeeMachineVer2/ViewController.swift
+++ coffeeMachineVer2/ViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class ViewController: UIViewController {
     
     var coffee = CoffeeMachine.init(waterPortions: 10, cofeePortions: 10, milkPortions: 10, donePortionsNumber: 10)
+    
+    let homeScreenWriting = "choose your drink"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        theInformationLabel.text = "choose your drink"
+        theInformationLabel.text = homeScreenWriting
+        currentState()
     }
     @IBOutlet weak var theAmericanoLabel: UILabel!
     
@@ -24,29 +27,189 @@ class ViewController: UIViewController {
     @IBOutlet weak var theInformationLabel: UILabel!
     
     @IBOutlet weak var theCapuchinoLabel: UILabel!
+    
+    @IBOutlet weak var coffeeLeftLabel: UILabel!
+    
+    @IBOutlet weak var coffeeNumberLeftLabel: UILabel!
+    
+    @IBOutlet weak var milkLeftNumberLabel: UILabel!
+    
+    @IBOutlet weak var waterLeftNumberLabel: UILabel!
+    
+    @IBOutlet weak var theUntilCleanLabel: UILabel!
+    
+    @IBOutlet weak var addWaterLabel: UILabel!
+    
+    @IBOutlet weak var addMilkLabel: UILabel!
+    
+    @IBOutlet weak var addCoffeeLabel: UILabel!
+    
+    
+    @IBAction func addWaterButton() {
+        if homeScreen() == true {
+            return
+        }
+        coffee.waterPortions = 10
+        currentState()
+    }
+    
+
+    @IBAction func addMilkButton() {
+        if homeScreen() == true {
+            return
+        }
+        coffee.milkPortions = 10
+        currentState()
+    }
+    
+    @IBAction func addCoffeeButton() {
+        if homeScreen() == true {
+            return
+        }
+        coffee.coffeePortions = 10
+        currentState()
+    }
+    
+    
+    @IBAction func theCleanButton() {
+        if homeScreen() == true {
+            return
+        }
+        coffee.donePortionsNumber = 10
+        currentState()
+    }
+    
+    
     @IBAction func theOkButton() {
         theInformationLabel.text = ""
         theEspressoLabel.text = "espresso"
         theAmericanoLabel.text = "americano"
+        theCapuchinoLabel.text = "capuchino"
+        addWaterLabel.text = "add water"
+        addMilkLabel.text = "add milk"
+        addCoffeeLabel.text = "add coffee"
     }
     
     @IBAction func theEspressoButton() {
-        theEspressoLabel.text = ""
-        theInformationLabel.text = "espresso works"
+        if homeScreen() == true {
+            return
+        }
+        cleanLabels()
+        if homeScreen() == true {
+            return
+        }
+        cleanLabels()
+        if checkIngredients() == false {
+            return
+        }
+        else {
+            coffee.espresso()
+            cleanLabels()
+            theInformationLabel.text = "your espresso"
+        }
+        currentState()
     }
     
     @IBAction func theAmericanoButton() {
-        theEspressoLabel.text = ""
-        theAmericanoLabel.text = ""
-        theInformationLabel.text = "americano"
+        if homeScreen() == true {
+            return
+        }
+        cleanLabels()
+        if homeScreen() == true {
+            return
+        }
+        cleanLabels()
+        if checkIngredients() == false {
+            return
+        }
+        else {
+            coffee.americano()
+            cleanLabels()
+            theInformationLabel.text = "your americano"
+        }
+        currentState()
     }
     
     @IBAction func theCapuchinoButton() {
+        if homeScreen() == true {
+            return
+        }
+        cleanLabels()
+        if checkIngredients() == false {
+            return
+        }
+        else {
+            coffee.capuccino()
+            cleanLabels()
+            theInformationLabel.text = "your capuchino"
+        }
+        currentState()
+    }
+    
+    func cleanLabels() {
+        theAmericanoLabel.text = ""
+        theEspressoLabel.text = ""
+        theCapuchinoLabel.text = ""
+        addWaterLabel.text = ""
+        addMilkLabel.text = ""
+        addCoffeeLabel.text = ""
+        
+    }
+    
+    func checkIngredients() -> Bool {
+        var makeCoffee = true
+        
+        if coffee.waterPortions == 0 {
+            cleanLabels()
+            theInformationLabel.text = "add whater"
+            makeCoffee = false
+            return makeCoffee
+        }
+        
+        if coffee.milkPortions == 0 {
+            cleanLabels()
+            theInformationLabel.text = "add milk"
+            makeCoffee = false
+            return makeCoffee
+        }
+        
+        if coffee.coffeePortions == 0 {
+            cleanLabels()
+            theInformationLabel.text = "add coffee"
+            makeCoffee = false
+            return makeCoffee
+        }
+        
+        if coffee.donePortionsNumber == 0 {
+            cleanLabels()
+            theInformationLabel.text = "clean me"
+            makeCoffee = false
+            return makeCoffee
+        }
+        
+        return makeCoffee
+    }
+    
+    func homeScreen() -> Bool {
+        var homeScreen = false
+        if theInformationLabel.text != "" {
+            homeScreen = true
+            return homeScreen
+        }
+        return homeScreen
+    }
+    
+    func currentState() {
+        coffeeNumberLeftLabel.text = String(coffee.coffeePortions)
+        milkLeftNumberLabel.text = String(coffee.milkPortions)
+        waterLeftNumberLabel.text = String(coffee.waterPortions)
+        theUntilCleanLabel.text = String(coffee.donePortionsNumber)
     }
     
     
-    
-    
+    func ifSomethingWrong() {
+        
+    }
     
     
     
